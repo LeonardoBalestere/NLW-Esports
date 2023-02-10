a6aa2471cf1d43557aeb7f721d06cf78750da9d1
Leonardo Balestere <alvesleonardo92@hotmail.com>
2023-02-10 20:44:00 -0300

    Adicionado parte mobile e att web

---
+++ web/src/App.tsx
@@ -0,0 +1,21 @@
+import './styles/main.css'
+
+interface ButtonProps {
+  title: string
+}
+
+function Button(props: ButtonProps) {
+  return <button>{props.title}</button>
+}
+
+function App() {
+  return (
+    <div>
+      <Button title= "Send1"/>    
+      <Button title= "Send2"/>    
+      <Button title= "Send3"/>    
+    </div>
+  )
+}
+
+export default App
